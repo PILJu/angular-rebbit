e14119550ff5b0b026faf6009deef6f8f688ee60
PILJu <elec0325@gmail.com>
2019-05-20 16:08:27 +0900

    처음 커밋

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,37 @@
 import { Component } from '@angular/core';
 
+import { Article } from './article/article.model';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'angular-reddit';
+
+  articles: Article[];
+
+  constructor() {
+    this.articles = [
+      new Article('Angular', 'http://angular.io', 3)
+    ];
+  }
+
+  addArticle(title: HTMLInputElement, link: HTMLInputElement): boolean {
+    try {
+      console.log(`Adding article title: ${title.value} and link: ${link.value}`);
+      this.articles.push(new Article(title.value, link.value, 0));
+      title.value = '';
+      link.value = '';
+    } catch (err) {
+      console.log(err);
+    }
+    return false;
+  }
+
+  sortedArticles(): Article[] {
+    return this.articles.sort((a: Article, b: Article) => b.votes - a.votes);
+  }
+
 }
